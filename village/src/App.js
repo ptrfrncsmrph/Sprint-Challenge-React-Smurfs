a0f44bf0c1a754c372b5cc2152fb08ef5b3c451f
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-01 11:58:56 -0500

    Runs Prettier, adds useEffect for Axios call

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.scss"
+import SmurfForm from "./components/SmurfForm"
+import Smurfs from "./components/Smurfs"
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+const App = () => {
+  const [smurfs, setSmurfs] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://10.0.0.53:3333/smurfs/")
+      .then(({ data }) => setSmurfs(data))
+  }, [])
+
+  return (
+    <div className="App">
+      <SmurfForm />
+      <Smurfs smurfs={smurfs} />
+    </div>
+  )
 }
 
-export default App;
+export default App
